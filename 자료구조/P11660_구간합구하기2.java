d5d60b967d87869e523799e9eedc95836ee4eba2
doitcodingtestjava <102801200+doitcodingtestjava@users.noreply.github.com>
2022-04-05 21:25:33 +0900

    Update and rename P11660_구간합구하기5.java to P11660_구간합구하기2.java

--- 자료구조/P11660_구간합구하기2.java
+++ 자료구조/P11660_구간합구하기2.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-public class P11660_±¸°£ÇÕ±¸ÇÏ±â5 {
+public class P11660_êµ¬ê°„í•©êµ¬í•˜ê¸°2 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,7 +17,7 @@ public class P11660_±¸°£ÇÕ±¸ÇÏ±â5 {
     int D[][] = new int[N + 1][N + 1];
     for (int i = 1; i <= N; i++) {
       for (int j = 1; j <= N; j++) {
-        //ºÎºÐ ÇÕ ±¸ÇÏ±â
+        //ë¶€ë¶„ í•© êµ¬í•˜ê¸°
         D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
       }
     }
@@ -27,7 +27,7 @@ public class P11660_±¸°£ÇÕ±¸ÇÏ±â5 {
       int y1 = Integer.parseInt(st.nextToken());
       int x2 = Integer.parseInt(st.nextToken());
       int y2 = Integer.parseInt(st.nextToken());
-      //  ºÎºÐ ÇÕ ¹è¿­À» ÀÌ¿ëÇÑ ÁúÀÇ ´äº¯ÇÏ±â
+      //  ë¶€ë¶„ í•© ë°°ì—´ì„ ì´ìš©í•œ ì§ˆì˜ ë‹µë³€í•˜ê¸°
       int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
       System.out.println(result);
     }
