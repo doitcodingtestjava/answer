aa23647442dcc50b422a4a1facf49daaaee964c3
doitcodingtestjava <102801200+doitcodingtestjava@users.noreply.github.com>
2022-04-17 03:37:43 +0900

    Add files via upload
    퀵정렬는 현재 시간 초과가 나는 부분이 있어 병합정렬 정답버전 올려드립니다. (문제자체가 퀵정렬로는 시간초과가 나는 문제입니다. 하지만 퀵정렬의 내부 로직에 대해서 정확하게 아는 것이 중요해 책에는 퀵정렬 로직으로 구현한 부분을 기재하였습니다.)

---
+++ 정렬/P11004_K번째수_시간초과없는버전.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11004_K번째수_시간초과없는버전 {
+  public static int[] A, tmp;
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(in.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(in.readLine());
+    A = new int[N + 1];
+    tmp = new int[N + 1];
+    for (int i = 1; i <=N; i++) {
+      A[i] = Integer.parseInt(st.nextToken());
+    }
+    merget_sort(1, N);
+    System.out.println(A[K]);
+  }
+  
+  private static void merget_sort(int s, int e) {
+    if (e - s < 1)
+      return;
+    int m = s + (e - s) / 2;
+    // 재귀함수 형태로 구현
+    merget_sort(s, m);
+    merget_sort(m + 1, e);
+    for (int i = s; i <= e; i++) {
+      tmp[i] = A[i];
+    }
+    int k = s;
+    int index1 = s;
+    int index2 = m + 1;
+    while (index1 <= m && index2 <= e) { // 두 그룹을 Merge 해주는 로직
+      if (tmp[index1] > tmp[index2]) {
+        A[k] = tmp[index2];
+        k++;
+        index2++;
+      } else {
+        A[k] = tmp[index1];
+        k++;
+        index1++;
+      }
+    }
+    // 한쪽 그룹이 모두 선택된 후 남아있는 값 정리하기
+    while (index1 <= m) {
+      A[k] = tmp[index1];
+      k++;
+      index1++;
+    }
+    while (index2 <= e) {
+      A[k] = tmp[index2];
+      k++;
+      index2++;
+    }
+
+  }
+}
