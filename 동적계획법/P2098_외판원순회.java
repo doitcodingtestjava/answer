a97d037bc44d408722dbaed524c3e6c92fa2c94f
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2022-07-29 10:22:09 +0900

    Update P2098_외판원순회.java

--- 동적계획법/P2098_외판원순회.java
+++ 동적계획법/P2098_외판원순회.java
@@ -2,25 +2,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 class P2098_외판원순회 {
-    private static final int INF = 1000000*16+1;
     private static int N;
     private static int[][] W;
     private static int[][] d;
+    private static final int INF = 1000000*16+1;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
         N = Integer.parseInt(br.readLine().trim());
         W = new int[16][16];
+        d = new int[16][1 << 16];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine().trim());
             for (int j = 0; j < N; j++) {
                 W[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        d = new int[16][1 << 16];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 1 << N; j++) {
-                d[i][j] = INF; //모든 비용을 충분히 큰수로 저장
             }
         }
         System.out.println(tsp(0, 1));
@@ -29,7 +24,7 @@ class P2098_외판원순회 {
         if (v == (1 << N) - 1) {    // 모든 노드를 방문한 경우
             return W[c][0] == 0 ? INF : W[c][0];
         }
-        if (d[c][v] != INF) {  //이미 방문한 노드인 경우 -> 이미 계산한 경우 바로 리턴 (메모이제이션)
+        if (d[c][v] != 0) {  //이미 방문한 노드인 경우 -> 이미 계산한 경우 바로 리턴 (메모이제이션)
             return d[c][v];
         }
         int min_val = INF;
