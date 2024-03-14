500c8f99e80f81849054a4584fcc1cfccc251990
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2024-03-14 20:30:09 +0900

    Update P1325_효율적인해킹.java

--- 그래프/P1325_효율적인해킹.java
+++ 그래프/P1325_효율적인해킹.java
@@ -1,23 +1,20 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.*;
 public class P1325_효율적인해킹 {
-    static int N, M;
-    static boolean visited[];
-    static int answer[];
-    static ArrayList<Integer> A[];
-    static int maxVal;
-    public static void main(String[] args) throws IOException {
+    static boolean visited[]  = new boolean[10001];;
+    static int answer[] = new int[10001];;
+    static ArrayList<Integer> A[] = new ArrayList[10001];;
+
+    static Queue<Integer> queue = new ArrayDeque<Integer>();
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        A = new ArrayList[N + 1];
-        answer = new int[N + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
         for (int i = 1; i <= N; i++)
-            A[i] = new ArrayList<Integer>();
+            A[i] = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
@@ -25,20 +22,28 @@ public class P1325_효율적인해킹 {
             A[S].add(E);
         }
         for (int i = 1; i <= N; i++) { //모든 정점에 대하여 BFS 실행
-            visited = new boolean[N + 1];
+            for (int j = 1; j <= N; j++) {
+                visited[j] = false;
+            }
+
             BFS(i);
         }
-      
+
+        int maxVal = 0;
         for (int i = 1; i <= N; i++) {
-            maxVal = Math.max(maxVal, answer[i]);
+            if (maxVal < answer[i]) {
+                maxVal = answer[i];
+            }
         }
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
             if (answer[i] == maxVal) //answer배열에서 maxVal와 같은 값을 가진 index를 정답으로 출력
-                System.out.print(i + " ");
+                sb.append(i).append(" ");
         }
+        System.out.println(sb);
     }
     public static void BFS(int index) {
-        Queue<Integer> queue = new ArrayDeque<Integer>();
         queue.add(index);
         visited[index] = true;
         while (!queue.isEmpty()) {
